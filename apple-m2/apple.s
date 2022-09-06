2ccecad41c52ef11aa1bf5817b8922533eb8fddc
Robert Hargreaves <kiyouta@gmail.com>
2022-09-06 22:18:37 +0100

    Print message

--- apple-m2/apple.s
+++ apple-m2/apple.s
@@ -1,8 +1,19 @@
+.data
+msg:
+	.ascii	"Hello, ARM!\n"
+len = . - msg
+
 .text
 .global _start
 _start:
-	/* syscall exit(int status) */
-	
+	/* syscall write(fd, buf, count) */
+	mov x0, #1 /* STDOUT */
+	ldr x1, =msg
+	ldr x2, =len
+	mov w8, #64 /* write */
+	svc #0 /* invoke */
+
+	/* syscall exit(status) */
 	mov x0, #0  /* status 0 */
 	mov x8, #93 /* exit */
-	svc #0 /* invoke syscall */
+	svc #0 /* invoke */
